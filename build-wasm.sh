e687dda35c983c4eaa9ed2a2443d16e0568f6bd2
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2020-10-26 12:43:20 +0100

    cleanup

--- build-wasm.sh
+++ build-wasm.sh
@@ -10,11 +10,12 @@ cd ..
 
 # build srtpdecrypt.wasm
 mkdir -p wasm/dist
+cd src
 ARGS=(
   -O3
   -s WASM=1
   -s ERROR_ON_UNDEFINED_SYMBOLS=0
-  -I. -I./libpcap-1.9.1
-  -o wasm/dist/rtptool.js aes.c analyze.c base64.c decrypt.c extract.c file.c hex.c srtpdecrypt.c usage-and-help.c
+  -I. -I../libpcap-1.9.1
+  -o ../wasm/dist/rtptool.js aes.c analyze.c base64.c decrypt.c extract.c file.c hex.c srtpdecrypt.c usage-and-help.c
 )
 emcc "${ARGS[@]}"
--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ ldconfig
 
 # build srtpdecrypt x86_64
 mkdir -p dist
-gcc aes.c analyze.c base64.c decrypt.c extract.c file.c hex.c srtpdecrypt.c usage-and-help.c -o dist/rtptool -lpcap
+cd src
+gcc aes.c analyze.c base64.c decrypt.c extract.c file.c hex.c srtpdecrypt.c usage-and-help.c -o ../dist/rtptool -lpcap
 
